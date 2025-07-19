85d353887b75ffe9a9b1952b209d4a1fe975d13c
AgusCharras <Agusccharras@gmail.com>
2025-07-18 21:21:36 -0300

    proyecto 2

--- uvpy/uvpy/settings.py
+++ uvpy/uvpy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lhs$=*o&q-yxeryin^$@gwu)9s_8v&_1*k))f-$f7u!ugnv-nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
@@ -114,7 +114,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+import os
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
