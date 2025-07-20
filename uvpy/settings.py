733409199e7221bb29d557f59092b28d012ba16e
AgusCharras <Agusccharras@gmail.com>
2025-07-20 13:16:09 -0300

    debug true

--- uvpy/settings.py
+++ uvpy/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lhs$=*o&q-yxeryin^$@gwu)9s_8v&_1*k))f-$f7u!ugnv-nr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-DEBUG = "False"
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+#DEBUG = "False"
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
